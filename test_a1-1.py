fe2665a005ecf0c4b5c0ca65ee212d5738efe031
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-09 20:32:25 +0100

    Update and rename test_a1-1.py to test_a1_1.py

--- test_a1-1.py
+++
@@ -1,4 +0,0 @@
-import a1-1-ada-lovelace
-
-def test_ada():
-    assert a1-1-ada-lovelace.ada() == "Ada Lovelace"
---
+++ test_a1_1.py
@@ -0,0 +1,4 @@
+import a1_1
+
+def test_ada():
+    assert a1_1.ada() == "Ada Lovelace"
