8f4338651abcecac5b1708b3799cefd82f2544ec
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-27 16:10:29 +0100

    Adds tests

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -4,4 +4,4 @@ from src.exercise import main
 def test_exercise(capsys):
     main()
     out, err = capsys.readouterr()
-    assert out == "\n", "Should read ''"
+    assert out == "Ada Lovelace\n", "Should read 'Ada Lovelace'"
